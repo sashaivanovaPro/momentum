3a64026d01a7f9d310caa7ef903fbc9837ad6a28
sashaivanovaPro <sashaivanova.pro@gmail.com>
2023-02-23 16:58:06 +0300

    feat: greating saving name

--- momentum/js/main.js
+++ momentum/js/main.js
@@ -1,18 +1,63 @@
 /* Time app */
 
 
-
 function currentTime() {
-    const date = new Date(); /* creating object of Date class */
-    const hour = date.getHours();
-    const min = date.getMinutes();
-    const sec = date.getSeconds();
-    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour); /* assigning hour in 12-hour format */
+    let date = new Date(); /* creating object of Date class */
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    /*hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour);  assigning hour in 12-hour format */
     hour = updateTime(hour);
     min = updateTime(min);
     sec = updateTime(sec);
     document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
     const t = setTimeout(currentTime, 1000); /* setting timer */
+
+    let year = date.getFullYear();
+    year = updateTime(year);
+    document.getElementById("year").innerText = year;
+
+
+    let days = ['Sunday', 'Monday', 'Tueseday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let months = [
+        'January',
+        'Fabruary',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+      ]
+    let day = days[date.getDay()];
+
+    let month = months[date.getMonth()];
+
+    let dateNumber = date.getDate();
+
+    document.getElementById("date").innerText = day + " , " + month + " " + dateNumber;
+
+    let greeting = ['Good morning','Good afternoon','Good evening','Good night'];
+
+    let sayHello;
+    if (hour < 6 ){
+        sayHello = greeting[3];
+  } else if (hour < 12 ){
+        sayHello = greeting[0];
+  } else if (hour < 18){
+        sayHello = greeting[1];
+  } else if (hour < 24){
+        sayHello =greeting[2];
+  };
+  let name = document.getElementById("input__name");
+
+  document.getElementById("greeting").innerText = sayHello + " , ";
+
   }
 
   function updateTime(k) { /* appending 0 before time elements if less than 10 */
@@ -26,51 +71,35 @@ function currentTime() {
 
   currentTime();
 
-  /* Date  */
+  /* Greeting save name after page reload*/
 
-  /*
-function currentDate() {
+const input = document.getElementById('input__name');
 
-    const year = date.getFullYear()
-    const date = date.getDate()
-    const months = [
-        'января',
-        'февраля',
-        'марта',
-        'апреля',
-        'мая',
-        'июня',
-        'июля',
-        'августа',
-        'сентября',
-        'октября',
-        'ноября',
-        'декабря'
-    ]
+console.log (input);
+function setLocalStorage() {
+    localStorage.setItem('name', input.value);
+  }
+window.addEventListener('beforeunload', setLocalStorage)
+console.log(input.value);
 
-    const monthName = months(date.getMonth())
-
-    const days = [
-        'Воскресенье',
-        'Понедельник',
-        'Вторник',
-        'Среда',
-        'Четверг',
-        'Пятница',
-        'Суббота'
-    ]
-
-    const monthName = months(date.getMonth())
-
-    const dayName = days[date.getDay()]
-
-//    const formatted = '${dayName}, ${date} ${monthName} ${year}'
-// Четверг, 23 января 2019
-document.getElementById("date").innerText = dayName + " , " + monthName + "   " + date;
-document.getElementById("year").innerText = year;
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    input.value = localStorage.getItem('name');
+  }
 }
+window.addEventListener('load', getLocalStorage)
 
-currentDate();
+/* Image change */
+
+
+
+
+
+
+
+
+
+
 
 
 
