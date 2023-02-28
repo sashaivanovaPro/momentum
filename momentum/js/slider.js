c480f9f5de9250f7dc210a9d60ef1af67e66d343
sashaivanovaPro <sashaivanova.pro@gmail.com>
2023-02-28 21:53:43 +0300

    feat: basis of slider functuion, put images one after another

--- momentum/js/slider.js
+++ momentum/js/slider.js
@@ -1,18 +1,20 @@
-console.log("Hello world!");
 
 /*  Load image from GitHub source */
 
-function getImage (){
-    let date = new Date();
-    let hour = date.getHours();
+let date = new Date();
+let hour = date.getHours(); // get actual hour to assigh day part
 
-let dayPart = ['morning','afternoon','evening','night'];
+let dayPart = ['morning','afternoon','evening','night']; // name of pickture arrow due to day part
 
-console.log(dayPart[1]);
+let choosePicture; // item from picture arrow
 
-let choosePicture;
+let plusImage = document.querySelector(".arrow_right") // find arrow right element
+console.log(plusImage);
 
-    if (hour < 6 ){
+let minusImage = document.querySelector(".arrow_left") // find arrow left element
+console.log(minusImage);
+
+if (hour < 6 ){
     choosePicture = dayPart[3];
   } else if (hour < 12 ){
     choosePicture = dayPart[0];
@@ -21,21 +23,68 @@ let choosePicture;
   } else if (hour < 24){
     choosePicture =dayPart[2];
   };
+  // algorithm for choosing picture arrow
 
-  function getRandomInt(min,max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    n = ((Math.floor(Math.random() * (max - min)) + min).toString()).padStart(2,"0");
-    return n
+    // random number between 1 and 20 from each arrow
+
+    function getRandomInt(min,max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        n = ((Math.floor(Math.random() * (max - min)) + min).toString()).padStart(2,"0");
+        console.log(n);
+        return n
+      }
+    let number = getRandomInt(2,20);
+
+    console.log (number);
+
+      // результат - number, переводим в строку и добавляем до 2 значного символа ноль, если это значение меньше 10
+    // min - попадает в рэндом, max - нет
+
+
+function getImage (){
+//ссылка прописана с обратными кавычками для формирования новой строки с учётом полученных данных из функции
+
+//link = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${choosePicture}/${n}.jpg')`;
+
+// add a new style to body element
+
+const img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${choosePicture}/${n}.jpg`;
+  img.onload = () => {
+    document.body.style.backgroundImage = `url(${img.src})`;;
+  };
+  console.log(img.src);
+
+function getSlideNext(){
+    if (number !== 20){
+        number = parseInt(number) + 1;
+    } else {
+        number = "01";
+    };
+    console.log((number.toString()).padStart(2,"0"));
+    return (number.toString()).padStart(2,"0");
   }
-  getRandomInt(1, 21);
 
-// n = (Math.floor(Math.random() * (max - min)) + min).toString.padStart(2,"0");
+  plusImage.onclick = function(){
+    getSlideNext();
+  }
+  // function to change image to next in arrow
 
-let body = document.querySelector(".body");
+  function getSlidePrevious(){
+    if (number !==1){
+        number = parseInt(number) - 1;
+    } else{
+        number = "20";
+    };
+    console.log((number.toString()).padStart(2,"0"));
+    return (number.toString()).padStart(2,"0");
+  }
 
-let link = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${choosePicture}/${n}.jpg')`;
-
-body.style.backgroundImage = link;
+  minusImage.onclick = function(){
+    getSlidePrevious();
+  }
+  // function to change image to previous in arrow
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${choosePicture}/${number}.jpg`;
 }
 getImage ()
